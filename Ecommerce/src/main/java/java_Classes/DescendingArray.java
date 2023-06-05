26cb9b69b77531ea6c1f2147ac77cff45723bbb6
Admin <tejasjadhav8888@gmail.com>
2023-06-05 23:17:27 +0530

    changes added on 05/-6/2-23

--- Ecommerce/src/main/java/java_Classes/DescendingArray.java
+++ Ecommerce/src/main/java/java_Classes/DescendingArray.java
@@ -7,7 +7,26 @@ import org.testng.annotations.Test;
 public class DescendingArray {
 	
 	@Test
-	
+	public static void ascendingDescending() {
+		int a[]= {10, 12, -9, 29, 35, -23};
+		Integer c[]= {10, 12, -9, 29, 35, -23};
+		Arrays.sort(a);
+
+		String b = " ";
+		for (int i=0; i<a.length; i++ )
+		{
+
+			b= b + " " + a[i] ;
+
+		}
+		System.out.println( " Ascending numbers = " + b);
+
+		Arrays.sort(c, Collections.reverseOrder());
+		System.out.println( " Descending numbers = " + Arrays.toString(c));
+	}
+
+
+	@Test
 	public void UsingSortMethod()
 	{
 		
